d2e60551f71ea0b1eef49920c827426e52466c27
bleidi <rafael.bleidi@gmail.com>
2019-01-02 18:24:44 -0200

    Division by zero example.

--- src/test/java/io/github/bleidi/AppTest.java
+++ src/test/java/io/github/bleidi/AppTest.java
@@ -14,6 +14,11 @@ public class AppTest {
 	private static final double DELTA = 0.0001d;
 
 	@Property
+	public void divisionDistributiveWithDivisonByZero(double n1, double n2,double n3) {
+		assertEquals((n1 + n2) / n3, n1 / n3 + n2 / n3, DELTA);
+	}
+
+	@Property
 	public void divisionDistributive(double n1, double n2, @InRange(min = "0") double n3) {
 		assertEquals((n1 + n2) / n3, n1 / n3 + n2 / n3, DELTA);
 	}
